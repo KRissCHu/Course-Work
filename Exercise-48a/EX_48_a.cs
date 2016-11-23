7333382715ac5725eac8156a71028be27052310b
KRissCHu <starkrisschucraft2@abv.bg>
2016-11-23 16:41:16 +0200

    This is the soluton to Exercise 48a.

---
+++ Exercise-48a/EX_48_a.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_48a
+{
+    class EX_48_a
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            int x = int.Parse(Console.ReadLine());
+
+            if(x<=0)
+            {
+                y = (5 * x) + 1;
+            }
+            else
+            {
+                y = 1 - x;
+            }
+        }
+    }
+}
